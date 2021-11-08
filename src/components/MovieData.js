a4209203a425d561afbf418af584ff4548d6f23b
Salim Khalfouni <salimkhalfouni23@gmail.com>
2021-11-08 22:15:26 +0000

    better than netflix

---
+++ src/components/MovieData.js
@@ -0,0 +1,94 @@
+
+
+const MovieData = [
+    {
+        id : 1,
+        title : "Dune" ,
+        description : "A mythic and emotionally charged hero's journey, Dune tells the story of Paul Atreides, a brilliant and gifted young man born into a great destiny beyond his understanding, who must travel to the most dangerous planet in the universe to ensure the future of his family and his people.",
+        posterURL : "https://m.media-amazon.com/images/I/517+uE+Ek2L.jpg",
+        trailer:"",
+        rating : 4
+    },
+    {
+        id : 2,
+        title : "Interstaller" ,
+        description : "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+        posterURL :"https://m.media-amazon.com/images/I/61iyi8V4ojL._AC_SL1058_.jpg",
+        rating : 5
+    },
+    {
+        id : 3,
+        title : "Arrival" ,
+        description : "A linguist works with the military to communicate with alien lifeforms after twelve mysterious spacecraft appear around the world.",
+        posterURL :"https://m.media-amazon.com/images/I/510PzUwEKKL._AC_.jpg",
+        rating : 4
+    },
+    {
+        id : 4,
+        title : "Sicario" ,
+        description : "An idealistic FBI agent is enlisted by a government task force to aid in the escalating war against drugs at the border area between the U.S. and Mexico.",
+        posterURL :"https://m.media-amazon.com/images/I/51XQ50xhJuL._AC_.jpg",
+        rating : 3
+    },
+    {
+        id : 5,
+        title : "Blade Runner 2049" ,
+        description : "Young Blade Runner K's discovery of a long-buried secret leads him to track down former Blade Runner Rick Deckard, who's been missing for thirty years.',    ",
+        posterURL :"https://m.media-amazon.com/images/I/51+Ev76740L._AC_.jpg",
+        rating : 4
+    },
+    {
+        id : 6,
+        title : "Batman v Superman : L'Aube de la justice" ,
+        description : "Fearing that the actions of Superman are left unchecked, Batman takes on the Man of Steel, while the world wrestles with what kind of a hero it really needs.",
+        posterURL :"https://m.media-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_FMjpg_UX1000_.jpg",
+        rating : 2
+    },
+    {
+        id :7,
+        title: 'Prisoners',
+
+        posterURL:
+          'https://m.media-amazon.com/images/I/41A8mioTXlL._AC_.jpg',
+        description:
+          'When Keller Dover daughter and her friend go missing, he takes matters into his own hands as the police pursue multiple leads and the pressure mounts.',
+        rating: 4,
+        },
+      {
+        id : 8,
+        posterURL:
+          'https://m.media-amazon.com/images/I/61TEii062+L._AC_.jpg',
+        title: 'The Grand Budapest Hotel',
+        rating: 3,
+        description:
+          'A writer encounters the owner of an aging high-class hotel, who tells him of his early years serving as a lobby boy in the hotels glorious years under an exceptional concierge.',
+      },
+      {
+        id : 9,
+        posterURL:
+          'https://m.media-amazon.com/images/M/MV5BYTcxYTA0MjAtYzdjNC00ZmFmLTgwYWItMmVhYTY5ZGZhOWQzXkEyXkFqcGdeQXVyMTAyOTE2ODg0._V1_.jpg',
+        title: 'The Shawshank Redemption',
+        rating: 5,
+        description:
+          'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
+      },
+      
+        {
+          id : 10,
+          posterURL:
+            'https://m.media-amazon.com/images/I/510L5ypQBdL._AC_SL1024_.jpg',
+          title: 'The Godfather',
+          rating: 5,
+          description:
+            'The aging patriarch of an organized crime dynasty in postwar New York City transfers control of his clandestine empire to his reluctant youngest son.',
+        },
+       
+    
+    
+    
+
+    
+
+]
+
+export default MovieData
